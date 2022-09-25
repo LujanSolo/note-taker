245e2eb91bd94d3c7ff9b388f4241972106ec220
LujanSolo <lujansolo@proton.me>
2022-09-24 19:23:58 -0700

    added index GET route

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -3,7 +3,16 @@ const router = require('express').Router();
 const fs = require('fs');
 
 //todo DISPLAY THE INDEX.HTML
+router.get("/", async (req, res) => {
 
+
+    if(err){
+      res.json(err);
+    } else{
+      const index = JSON.parse(data)
+      res.json(index);
+    }
+}
 
 
 
