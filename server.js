6e9193a1e8a9d357918327ca4e127835c6859447
LujanSolo <lujansolo@proton.me>
2022-09-24 18:56:53 -0700

    1. Organized file structure. 2. Added javascript files for routes. 3. Built out basic server routes on server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require("path");
+
+const routes = require('./routes');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"))
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+
+app.use(routes);
+
+app.listen(PORT, () => console.log('Server now running...'));
