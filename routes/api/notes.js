f269723b7d08644c67583e7ddadbe66d86a570a5
LujanSolo <lujansolo@proton.me>
2022-09-24 19:28:08 -0700

    modified index GET route to readFile to db.json

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -5,15 +5,15 @@ const fs = require('fs');
 //todo DISPLAY THE INDEX.HTML
 router.get("/", async (req, res) => {
 
-
+  fs.readFile("./db/db.json", "utf8", (err,data)=>{
     if(err){
       res.json(err);
     } else{
       const index = JSON.parse(data)
       res.json(index);
     }
-}
-
+  })
+});
 
 
 //TODO DISPLAY NOTES.HTML
